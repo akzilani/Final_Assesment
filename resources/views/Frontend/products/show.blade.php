dd48cfc3515da0638dac489de2b8a3755ff25ef9
zilani <dreammission2021@gmail.com>
2022-09-11 10:33:12 +0600

    update

--- resources/views/Frontend/products/show.blade.php
+++ resources/views/Frontend/products/show.blade.php
@@ -9,33 +9,37 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-    <button class="btn btn-primary m-auto d-block mt-5 mb-5">Product Details Information</button>
+    <button class="btn btn-warning m-auto d-block mt-5 mb-5 " style="font-size: 20px">Product Details Information</button>
     <div class="card">
         <div class="card-body">
             <div class="table-responsive mt-3">
-                <table class="table table-border custom-table-css table-border table-hover">
-                    <tbody>
+                <table class="table table-border custom-table-css table-border table-hover table-dark table-striped">
+                    <tbody class="">
+                        <tr class="table-danger fw-bold">
+                            <th>Section</th>
+                            <td>Details</td>
+                        </tr>
                         <tr>
-                            <th scope="row">Name</th>
+                            <th scope="row">Name:</th>
                             <td>{{$products->name}}</td>
                         </tr>
                         <tr>
-                            <th scope="row">Category </th>
+                            <th scope="row">Category :</th>
                             <td>{{$products->category_name}}</td>
                         </tr>
 
                         <tr>
-                            <th scope="row"> Brand Name</th>
+                            <th scope="row"> Brand Name :</th>
                             <td>{{$products->brand_name}}</td>
                         </tr>
 
                         <tr>
-                            <th scope="row">Description</th>
+                            <th scope="row">Description :</th>
                             <td>{{$products->description}}</td>
                         </tr>
 
                         <tr>
-                            <th scope="row">Image</th>
+                            <th scope="row">Image :</th>
                             <td><img src="{{asset($products->image)}}"style="width: 120px; alt=""></td>
                         </tr>
                     </tbody>
